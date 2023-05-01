0d0962decac81e92f070e7388a7374699510171e
Kevand98 <125034003+Kevand98@users.noreply.github.com>
2023-05-01 11:56:53 +0200

    testa på andra datorn

--- model/model.py
+++ model/model.py
@@ -1,72 +1,79 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms as transforms
 
 class CNN(nn.Module):
     def __init__(self, class_count: int = 2, dropout: float = 0.8, drop_in: float = 0.9, device: str = 'cpu'):
         super().__init__()
 
-
-        
-        self.A1 = nn.Sequential(
-            nn.Conv2d(1, 300, 3, padding='same'),
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2),
         )
 
-        self.A2 = nn.Sequential(
-            nn.Conv2d(1, 300, 5, padding='same'),
-            nn.MaxPool2d(2, 2),
-        )
-
-        self.A3 = nn.Sequential(
-            nn.Conv2d(1, 300, 7, padding='same'),
-            nn.MaxPool2d(2, 2),
-        )
-
-        self.B1 = nn.Sequential(
-            nn.Conv2d(900, 100, 3),     # B1
-            nn.MaxPool2d(3, 3),
-            nn.Conv2d(100, 50, 3),
-            nn.MaxPool2d(4, 4)
-        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
 
         self.classifier = nn.Sequential(
-            nn.Dropout(p=drop_in),
-            
-            nn.Linear(3200, 1_000),                  #fc1
-            nn.LeakyReLU(),
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
             nn.Dropout(p=dropout),
-
-            nn.Linear(1_000, 1_000),
-            nn.LeakyReLU(),
+            nn.Linear(4096, 2048),
+            nn.ReLU(inplace=True),
             nn.Dropout(p=dropout),
-
-            nn.Linear(1_000, 1_000),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(1_000, 1_000),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(1_000, 100),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(100, class_count),             #fc5
+            nn.Linear(2048, class_count),
         )
 
-        self.to(device)
-        
+        self.dropout = nn.Dropout(p=dropout)
+        self.softmax = nn.Softmax(dim=1)
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        xA1 = self.A1(x)
-        xA2 = self.A2(x)
-        xA3 = self.A3(x)
-
-        A = torch.cat((xA1, xA2, xA3), dim=1)
-
-        B = self.B1(A)
-
-        x = torch.flatten(B, 1)
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
         x = self.classifier(x)
-        return x
+        x = self.dropout(x)
+        x = self.softmax(x)
 
+        return x
