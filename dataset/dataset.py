c959dae62cf310406687a1af408b534ce45e0577
Lucke0011 <lukas.ledin@gmail.com>
2023-05-05 13:55:23 +0200

    ?

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -17,22 +17,10 @@ def load_dataset(scale: list = [224, 224], batch_size: int = 10):
     valset = datasets.ImageFolder('dataset/chest_xray/val', transform)
     testset = datasets.ImageFolder('dataset/chest_xray/test', transform)
 
-    class_labels = trainset.targets
-    num_normal = class_labels.count(0)
-    num_pneumonia = class_labels.count(1)
-    min_len = min(num_normal, num_pneumonia)
-    labels = [0] * min_len + [1] * min_len
-    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
-
+    weights, labels = sample(trainset, 1)
     train_sampler = WeightedRandomSampler(weights, len(labels), replacement=False)
 
-    class_labels = testset.targets
-    num_normal = class_labels.count(0)
-    num_pneumonia = class_labels.count(1)
-    min_len = min(num_normal, num_pneumonia)
-    labels = [0] * min_len + [1] * min_len
-    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
-
+    weights, labels = sample(testset, 1)
     test_sampler = WeightedRandomSampler(weights, len(labels), replacement=False)
 
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, sampler=train_sampler, num_workers=2)
@@ -40,3 +28,15 @@ def load_dataset(scale: list = [224, 224], batch_size: int = 10):
     test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, sampler=test_sampler, num_workers=2)
 
     return train_loader, validation_loader, test_loader
+
+
+# size is the % of total dataset. Ex. 1 = 100% and 0.5 = 50%
+def sample(dataset, size):
+    class_labels = dataset.targets
+    num_normal = class_labels.count(0)
+    num_pneumonia = class_labels.count(1)
+    min_len = min(num_normal, num_pneumonia) * size
+    labels = [0] * min_len + [1] * min_len
+    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
+
+    return weights, labels
--- main.py
+++ main.py
@@ -27,5 +27,31 @@ def main(epochs: int = 4, batch_size: int = 8, learning_rate: float = 1e-4):
 
     print("\nTest accuracy: %f" % test_accuracy, "\nTest f1: %f" % f1)
 
+
+import os
+import shutil
+def hack():
+    # Set the path to the folder containing the pneumonia images
+    pneumonia_folder_path = "dataset/chest_xray_pneumonia/train/PNEUMONIA"
+
+    # Set the paths to the new viral and bacterial folders
+    viral_folder_path = "dataset/chest_xray_pneumonia/train/viral"
+    bacterial_folder_path = "dataset/chest_xray_pneumonia/train/bacterial"
+
+    # Create the new viral and bacterial folders if they don't already exist
+    os.makedirs(viral_folder_path, exist_ok=True)
+    os.makedirs(bacterial_folder_path, exist_ok=True)
+
+    # Loop through the pneumonia folder and move the images to the appropriate folder
+    for filename in os.listdir(pneumonia_folder_path):
+        if "virus" in filename:
+            shutil.move(os.path.join(pneumonia_folder_path, filename), os.path.join(viral_folder_path, filename))
+        elif "bacteria" in filename:
+            shutil.move(os.path.join(pneumonia_folder_path, filename), os.path.join(bacterial_folder_path, filename))
+
+    return
+
+
 if __name__=="__main__":
-    main()
+    # main()
+    hack()
