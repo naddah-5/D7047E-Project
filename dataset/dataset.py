c92b8cd87b211ce0daeec966495ad358a7967384
Lucke0011 <lukas.ledin@gmail.com>
2023-05-05 15:48:44 +0200

    samples for 3 classes

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -17,10 +17,10 @@ def load_dataset(scale: list = [224, 224], batch_size: int = 10):
     valset = datasets.ImageFolder('dataset/chest_xray_pneumonia/val', transform)
     testset = datasets.ImageFolder('dataset/chest_xray_pneumonia/test', transform)
 
-    weights, labels = sample(trainset, 1)
+    weights, labels = sample3(trainset, 1)
     train_sampler = WeightedRandomSampler(weights, len(labels), replacement=False)
 
-    weights, labels = sample(testset, 1)
+    weights, labels = sample3(testset, 1)
     test_sampler = WeightedRandomSampler(weights, len(labels), replacement=False)
 
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, sampler=train_sampler, num_workers=2)
@@ -31,12 +31,23 @@ def load_dataset(scale: list = [224, 224], batch_size: int = 10):
 
 
 # size is the % of total dataset. Ex. 1 = 100% and 0.5 = 50%
-def sample(dataset, size):
+def sample2(dataset, size):
     class_labels = dataset.targets
     num_class0 = class_labels.count(0)
     num_class1 = class_labels.count(1)
     min_len = min(num_class0, num_class1) * size
     labels = [0] * min_len + [1] * min_len
     weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
+
+    return weights, labels
+
+def sample3(dataset, size):
+    class_labels = dataset.targets
+    num_class0 = class_labels.count(0)
+    num_class1 = class_labels.count(1)
+    num_class2 = class_labels.count(2)
+    min_len = min(num_class0, num_class1, num_class2) * size
+    labels = [0] * min_len + [1] * min_len + [2] * min_len
+    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 3)
 
     return weights, labels
