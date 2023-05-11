9f50f7ea12f88fcb195fa8bf3273dfbd835488be
naddah-5 <andersoskardahl@hotmail.com>
2023-05-11 13:29:48 +0200

    might have fixed the skip-layer bug

--- model/model.py
+++ model/model.py
@@ -8,20 +8,55 @@ class CNN(nn.Module):
 
 
         
-        self.A1 = nn.Sequential(
+        self.A0 = nn.Sequential(
             nn.Conv2d(1, 100, 3),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(100, 200, 5),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(200, 300, 7),
-            nn.MaxPool2d(2, 2),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.A1 = nn.Sequential(
+            nn.Conv2d(100, 100, 3, padding='same'),
+        )
+
+        self.A2 = nn.Sequential(
+            nn.Conv2d(100, 100, 3),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.A3 = nn.Sequential(
+            nn.Conv2d(100, 100, 3, padding='same'),
+        )
+
+        self.A4 = nn.Sequential(
+            nn.Conv2d(100, 100, 3),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.A5 = nn.Sequential(
+            nn.Conv2d(100, 100, 3, padding='same'),
+        )
+
+        self.A6 = nn.Sequential(
+            nn.Conv2d(100, 100, 3),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.A7 = nn.Sequential(
+            nn.Conv2d(100, 100, 3, padding='same'),
+        )
+
+        self.A8 = nn.Sequential(
+            nn.Conv2d(100, 100, 3),
+        )
+
+        self.A9 = nn.Sequential(
+            nn.Conv2d(100, 100, 3, padding='same'),
         )
 
         self.B1 = nn.Sequential(
-            nn.Conv2d(300, 600, 3),                 # B1
-            nn.Dropout2d(p=0.2),
+            nn.Conv2d(100, 200, 3),                 # B1
+            nn.Dropout2d(p=0.3),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(600, 600, 3),
+            nn.Conv2d(200, 50, 3),
             nn.Dropout2d(p=0.2),
             nn.MaxPool2d(2, 2)
         )
@@ -29,7 +64,7 @@ class CNN(nn.Module):
         self.classifier = nn.Sequential(
             nn.Dropout(p=drop_in),
             
-            nn.Linear(9_600, 1_000),                 # fc1
+            nn.Linear(50, 1_000),                 # fc1
             nn.LeakyReLU(),
             nn.Dropout(p=dropout),
 
@@ -55,11 +90,44 @@ class CNN(nn.Module):
         self.to(device)
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.A0(x)
+        z1 = copy.copy(x)
+
         x = self.A1(x)
 
-        x = self.B1(x)
+        x = torch.add(x, z1)
+        x = torch.div(x, 2)
+        x = self.A2(x)
+        z2 = copy.copy(x)
 
-        x = torch.flatten(x, 1)
+        x = self.A3(x)
+
+        x = torch.add(x, z2)
+        x = self.A4(x)
+        z3 = copy.copy(x)
+
+        x = self.A5(x)
+        
+        x = torch.add(x, z3)
+        x = torch.div(x, 2)
+        x = self.A6(x)
+        z4 = copy.copy(x)
+
+        x = self.A7(x)
+
+        x = torch.add(x, z4)
+        x = torch.div(x, 2)
+        x = self.A8(x)
+        z5 = copy.copy(x)
+
+        x = self.A9(x)
+
+        x = torch.add(x, z5)
+        x = torch.div(x, 2)
+
+        B = self.B1(x)
+
+        x = torch.flatten(B, 1)
         x = self.classifier(x)
         return x
 
