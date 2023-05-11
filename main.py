5b2d124d88094366d9627800e97392367267fa26
Jim Agnevik <jimagn-8@student.ltu.se>
2023-05-11 18:31:57 +0200

    epoch

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dataset.dataset import load_dataset
 
 
 
-def main(epochs: int = 500, batch_size: int = 3, learning_rate: float = 1e-4):
+def main(epochs: int = 1, batch_size: int = 3, learning_rate: float = 1e-4):
     best_net: str = ''
 
     my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # set device to gpu if available
@@ -26,9 +26,9 @@ def main(epochs: int = 500, batch_size: int = 3, learning_rate: float = 1e-4):
     training.train_model() # make sure to use the right trainer train_model for resnet and regular model, train_model_inception for inception netz
     
 
-#    test_accuracy, f1 = test_model(test_loader=test_loader, network=model, device=my_device)
+    test_accuracy, f1 = test_model(test_loader=test_loader, network=model, device=my_device)
 
-#    print("\nTest accuracy: %f" % test_accuracy, "\nTest f1: %f" % f1)
+    print("\nTest accuracy: %f" % test_accuracy, "\nTest f1: %f" % f1)
 
 if __name__=="__main__":
     main()
