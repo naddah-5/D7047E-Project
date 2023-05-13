78c3ae3d13daa8af215b92e4de2406a3beb93ec8
naddah-5 <andersoskardahl@hotmail.com>
2023-05-13 12:13:35 +0200

    wip

--- model/model.py
+++ model/model.py
@@ -3,37 +3,34 @@ import torch.nn as nn
 import copy
 
 class CNN(nn.Module):
-    def __init__(self, class_count: int = 2, dropout: float = 0.8, drop_in: float = 0.9, device: str = 'cpu'):
+    def __init__(self, class_count: int = 2, dropout: float = 0.5, drop_in: float = 0.8, device: str = 'cpu'):
         super().__init__()
 
 
         
         self.A1 = nn.Sequential(
             nn.Conv2d(1, 1, 1),
-            nn.Conv2d(1, 200, 7),
-            nn.Dropout2d(p=0.1),
+            nn.Conv2d(1, 100, 7),
             nn.MaxPool2d(3, 3),
-            nn.Conv2d(200, 200, 5),
-            nn.Dropout2d(p=0.1),
+            nn.Conv2d(100, 100, 5),
             nn.MaxPool2d(3, 3),
-            nn.Conv2d(200, 200, 3),
-            nn.Dropout2d(p=0.1),
-            nn.MaxPool2d(2, 2),
+            nn.Conv2d(100, 100, 3),
         )
 
         self.B1 = nn.Sequential(
-            nn.Conv2d(200, 500, 3),                 # B1
-            nn.Dropout2d(p=0.8),
+            nn.Conv2d(100, 200, 5),                 # B1
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(500, 500, 3),
-            nn.Dropout2d(p=0.8),
+            nn.Conv2d(200, 200, 3),
             nn.MaxPool2d(2, 2)
         )
 
         self.classifier = nn.Sequential(
-            nn.Dropout(p=drop_in),
             
-            nn.Linear(500, 8_192),                 # fc1
+            nn.Linear(1_800, 8_192),                 # fc1
+            nn.LeakyReLU(),
+            nn.Dropout(p=drop_in),
+
+            nn.Linear(8_192, 8_192),
             nn.LeakyReLU(),
             nn.Dropout(p=dropout),
 
@@ -41,19 +38,19 @@ class CNN(nn.Module):
             nn.LeakyReLU(),
             nn.Dropout(p=dropout),
 
-            nn.Linear(8_192, 4_096),
+            nn.Linear(8_192, 8_192),
             nn.LeakyReLU(),
             nn.Dropout(p=dropout),
 
-            nn.Linear(4_096, 4_096),
+            nn.Linear(8_192, 1_024),
             nn.LeakyReLU(),
             nn.Dropout(p=dropout),
 
-            nn.Linear(4096, 1_000),
+            nn.Linear(1_024, 128),
             nn.LeakyReLU(),
             nn.Dropout(p=dropout),
 
-            nn.Linear(1_000, class_count),             # fc5
+            nn.Linear(128, class_count),             # fc5
         )
 
         self.to(device)
