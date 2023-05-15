3a5149422ad628e161e66345c335478c2b33fbf8
Kevand98 <125034003+Kevand98@users.noreply.github.com>
2023-05-15 23:22:55 +0200

    Skip layer, dålig....

--- model/model.py
+++ model/model.py
@@ -1,79 +1,56 @@
 import torch
 import torch.nn as nn
-import torchvision.transforms as transforms
 
 class CNN(nn.Module):
-    def __init__(self, class_count: int = 2, dropout: float = 0.8, drop_in: float = 0.9, device: str = 'cpu'):
+    def __init__(self, class_count: int = 2, dropout: float = 0.5, drop_in: float = 0.8, device: str = 'cpu'):
         super().__init__()
 
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+        self.features1 = nn.Sequential(
+            nn.Conv2d(1, 32, 5),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(256, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
+            nn.ReLU()
         )
-
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        
+        self.features2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU()
+        )
+        
+        self.features3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3),
+            #nn.MaxPool2d(2, 2),
+            nn.ReLU()
+        )
+        
+        self.skipLayer = nn.Sequential(
+            nn.Conv2d(32, 128, 7),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU()
+        )
 
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(inplace=True),
+            nn.Dropout(p=drop_in),
+            nn.Linear(346112, 100),
+            nn.LeakyReLU(),
             nn.Dropout(p=dropout),
-            nn.Linear(4096, 2048),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
-            nn.Linear(2048, class_count),
+            nn.Linear(100, class_count),
         )
 
-        self.dropout = nn.Dropout(p=dropout)
-        self.softmax = nn.Softmax(dim=1)
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
-        x = self.avgpool(x)
+
+        x = self.features1(x)
+        Residual = x
+
+        x = self.features2(x)
+        x = self.features3(x)
+
+        Residual = self.skipLayer(Residual)  
+        #print(Residual.shape)
+        
+        #print(x.shape)
+        x = x + Residual
+
         x = torch.flatten(x, 1)
         x = self.classifier(x)
-        x = self.dropout(x)
-        x = self.softmax(x)
-
         return x
