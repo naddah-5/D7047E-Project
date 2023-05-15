fdefac0c1db0542e17c17e3b1496ebb6a9f05278
Kevand98 <125034003+Kevand98@users.noreply.github.com>
2023-05-15 23:23:53 +0200

    Skip layer, dålig....

--- application/train.py
+++ application/train.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import f1_score
+from torch.optim.lr_scheduler import StepLR
 
 from .validate import validate_model
 
@@ -14,7 +16,8 @@ class Training():
         self.test_loader = test_loader
         self.epochs = epochs
         self.loss_function = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(network.parameters(), lr=learning_rate)
+        self.optimizer = optim.Adam(network.parameters(), lr=learning_rate, weight_decay=1e-5)
+        self.scheduler = scheduler = StepLR(self.optimizer, step_size=5, gamma=0.1)
         self.writer = SummaryWriter()
         self.best_net = best_net
         self.device=device
@@ -29,6 +32,10 @@ class Training():
             correct = 0
             total = 0
             accuracy = 0
+
+            y_true = []
+            y_pred = []
+
             for batch_nr, (data, labels) in enumerate(self.train_loader):
                 self.network.train()
                 iteration += 1
@@ -39,6 +46,9 @@ class Training():
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
+                y_true += labels.cpu().numpy().tolist()
+                y_pred += predicted.cpu().numpy().tolist()
+
                 self.predictions.append(predicted)
 
                 loss = self.loss_function(predictions, labels)
@@ -46,27 +56,32 @@ class Training():
 
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-
+                self.scheduler.step()
                 print(
                     f'\rEpoch {epoch + 1}/{self.epochs} [{batch_nr + 1}/{len(self.train_loader)}] - Loss {loss:.10f}' ,
                     end=''
                 )
 
             accuracy = correct / total
+            f1 = f1_score(y_true, y_pred)
             self.writer.add_scalar('Loss/train', loss, (epoch + 1))
             self.writer.add_scalar('Accuracy/train', accuracy, (epoch + 1))
+            self.writer.add_scalar('f1/train', f1, (epoch + 1))
+
+            loss, accuracy, f1 = validate_model(val_loader=self.val_loader, loss_function=self.loss_function, network=self.network, device=self.device)
 
             if self.debug_prediction:
                 for i in range(10):
                     print(self.predictions[-i])
 
-            loss, accuracy = validate_model(val_loader=self.val_loader, loss_function=self.loss_function, network=self.network, device=self.device)
+            loss, accuracy, _ = validate_model(val_loader=self.val_loader, loss_function=self.loss_function, network=self.network, device=self.device)
             if accuracy > best_accuracy:
                 best_accuracy = accuracy
                 torch.save(self.network.state_dict(), "best_network.pt")
                 print("\nFound better network, accuracy -", accuracy)
             self.writer.add_scalar('Loss/validation', loss, (epoch + 1))
             self.writer.add_scalar('Accuracy/validation', accuracy, (epoch + 1))
+            self.writer.add_scalar('f1/validation', f1, (epoch + 1))
 
         print("\nBest model had a validation accuracy of -", best_accuracy)
 
