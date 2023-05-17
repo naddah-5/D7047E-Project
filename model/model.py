284392be7b522daa5117a43aef7548908e8cfd85
Kevand98 <125034003+Kevand98@users.noreply.github.com>
2023-05-17 22:35:03 +0200

    ...

--- model/model.py
+++ model/model.py
@@ -7,50 +7,104 @@ class CNN(nn.Module):
 
         self.features1 = nn.Sequential(
             nn.Conv2d(1, 32, 5),
-            nn.MaxPool2d(2, 2),
-            nn.ReLU()
+            nn.BatchNorm2d(32),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
         )
         
         self.features2 = nn.Sequential(
-            nn.Conv2d(32, 64, 3),
-            nn.MaxPool2d(2, 2),
-            nn.ReLU()
+            nn.Conv2d(32, 64, 5, padding='same'),
+            nn.BatchNorm2d(64),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
         )
         
         self.features3 = nn.Sequential(
-            nn.Conv2d(64, 128, 3),
-            #nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 5, padding='same'),
+            nn.BatchNorm2d(128),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 100, 5, padding='same'),
+            nn.BatchNorm2d(100),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        
+        self.skipLayer1 = nn.Sequential(
+            nn.Conv2d(32, 128, 3, padding='same'),
+            nn.BatchNorm2d(128),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 100, 3, padding='same'),
+            nn.BatchNorm2d(100),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(4, 4)
+        )
+
+        self.skipLayer2 = nn.Sequential(
+            nn.Conv2d(32, 128, 7, padding='same'),
+            nn.BatchNorm2d(128),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 100, 7, padding='same'),
+            nn.BatchNorm2d(100),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(4, 4)
+        )
+
+        self.combine = nn.Sequential(
+            nn.Conv2d(300, 100, 3),
+            nn.BatchNorm2d(100),  # Added batch normalization
+            nn.ReLU(),
+            nn.MaxPool2d(3, 3),
+            nn.Conv2d(100, 50, 3),
+            nn.BatchNorm2d(50),  # Added batch normalization
             nn.ReLU()
         )
         
-        self.skipLayer = nn.Sequential(
-            nn.Conv2d(32, 128, 7),
-            nn.MaxPool2d(2, 2),
-            nn.ReLU()
-        )
-
         self.classifier = nn.Sequential(
-            nn.Dropout(p=drop_in),
-            nn.Linear(346112, 100),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-            nn.Linear(100, class_count),
+            #nn.Dropout(p=drop_in),
+            nn.Linear(16900, 8000),
+            nn.ReLU(),
+            #nn.Dropout(p=dropout),
+            nn.Linear(8000, 1000),
+            nn.ReLU(),
+            #nn.Dropout(p=dropout),
+            nn.Linear(1000, 100),
+            nn.ReLU(),
+            #nn.Dropout(p=dropout),
+            nn.Linear(100, class_count)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
+
         x = self.features1(x)
-        Residual = x
+        skip7x7 = x
+        skip3x3 = x
+        
+        skip3x3 = self.skipLayer1(skip3x3)  
+        #skip7x7 = self.skipLayer2(skip7x7)
 
         x = self.features2(x)
         x = self.features3(x)
 
-        Residual = self.skipLayer(Residual)  
+        #Residual = self.skipLayer(Residual)  
         #print(Residual.shape)
         
         #print(x.shape)
-        x = x + Residual
+        #print(skip3x3.shape)
+        #print(skip7x7.shape)
+        
+        #comb = torch.cat((x, skip3x3, skip7x7), dim=1)
+        #print(x.shape)
+        
+        #x = x + Residual
+        #comb = self.combine(comb)
+        #comb = (x + skip3x3 + skip7x7) / 3
+        comb = (x + skip3x3) / 2
+        #print(comb.shape)
 
-        x = torch.flatten(x, 1)
+        x = torch.flatten(comb, 1)
         x = self.classifier(x)
         return x
