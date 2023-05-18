fixed missmatch and memory overflow

model/model.py
@@ -13,51 +13,51 @@ class CNN(nn.Module):
         self.FeatureA1 = nn.Sequential(
             nn.Conv2d(1, 10, 1),
             nn.BatchNorm2d(10),
-            nn.Conv2d(10, 500, 5),
-            nn.BatchNorm2d(500),
+            nn.Conv2d(10, 400, 5),
+            nn.BatchNorm2d(400),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
             
-            nn.Conv2d(500, 10, 1),
+            nn.Conv2d(400, 10, 1),
             nn.BatchNorm2d(10),
             nn.LeakyReLU(),
         )
 
 
         self.FeatureA2 = nn.Sequential(
-            nn.Conv2d(10, 500, 5),
-            nn.BatchNorm2d(500),
+            nn.Conv2d(10, 400, 5),
+            nn.BatchNorm2d(400),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
 
-            nn.Conv2d(500, 10, 1),
+            nn.Conv2d(400, 10, 1),
             nn.BatchNorm2d(10),
             nn.LeakyReLU(),
             )
 
 
         self.FeatureA3 = nn.Sequential(
-            nn.Conv2d(10, 500, 5),
-            nn.BatchNorm2d(500),
+            nn.Conv2d(10, 400, 5),
+            nn.BatchNorm2d(400),
             nn.LeakyReLU(),
 
-            nn.Conv2d(500, 10, 1),
+            nn.Conv2d(400, 10, 1),
             nn.BatchNorm2d(10),
             nn.LeakyReLU(),
         )
 
         self.FeatureA4 = nn.Sequential(
-            nn.Conv2d(10, 500, 5),
-            nn.BatchNorm2d(500),
+            nn.Conv2d(10, 400, 5),
+            nn.BatchNorm2d(400),
             nn.LeakyReLU(),
 
-            nn.Conv2d(500, 10, 1),
-            nn.BatchNorm2d(10),
+            nn.Conv2d(400, 100, 1),
+            nn.BatchNorm2d(100),
             nn.LeakyReLU(),
         )
 
         self.classifierA1 = nn.Sequential(
-            nn.Linear(1_000, 1_800),
+            nn.Linear(1_600, 1_800),
             nn.LeakyReLU(),
         )
 