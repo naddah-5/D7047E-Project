bb0f8d47535b7e1aa18cd38627220780c2d717e0
naddah-5 <andersoskardahl@hotmail.com>
2023-05-18 21:51:03 +0200

    needs to be tested

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import copy
 
 class CNN(nn.Module):
-    def __init__(self, class_count: int = 2, dropout: float = 0.6, drop_in: float = 0.8, drop_tail = 0.4, device: str = 'cpu'):
+    def __init__(self, class_count: int = 2, dropout: float = 0.6, drop_in: float = 0.8, drop_tail = 0.5, device: str = 'cpu'):
         super().__init__()
 
         self.dropin = nn.Dropout(p=drop_in)
@@ -11,64 +11,74 @@ class CNN(nn.Module):
         self.droptail = nn.Dropout(p=drop_tail)
         
         self.FeatureA1 = nn.Sequential(
-            nn.Conv2d(1, 1, 1),
-            nn.BatchNorm2d(1),
-            nn.Conv2d(1, 200, 7),
-            nn.BatchNorm2d(200),
+            nn.Conv2d(1, 10, 1),
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 500, 5),
+            nn.BatchNorm2d(500),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
             
-            nn.Conv2d(200, 1, 1),
-            nn.BatchNorm2d(1),
+            nn.Conv2d(500, 10, 1),
+            nn.BatchNorm2d(10),
             nn.LeakyReLU(),
         )
 
 
         self.FeatureA2 = nn.Sequential(
-            nn.Conv2d(1, 400, 5),
-            nn.BatchNorm2d(400),
+            nn.Conv2d(10, 500, 5),
+            nn.BatchNorm2d(500),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
 
-            nn.Conv2d(400, 1, 1),
-            nn.BatchNorm2d(1),
+            nn.Conv2d(500, 10, 1),
+            nn.BatchNorm2d(10),
             nn.LeakyReLU(),
             )
 
 
         self.FeatureA3 = nn.Sequential(
-            nn.Conv2d(1, 400, 3),
-            nn.BatchNorm2d(400),
+            nn.Conv2d(10, 500, 5),
+            nn.BatchNorm2d(500),
             nn.LeakyReLU(),
 
-            nn.Conv2d(400, 100, 1),
-            nn.BatchNorm2d(100),
+            nn.Conv2d(500, 10, 1),
+            nn.BatchNorm2d(10),
+            nn.LeakyReLU(),
+        )
+
+        self.FeatureA4 = nn.Sequential(
+            nn.Conv2d(10, 500, 5),
+            nn.BatchNorm2d(500),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(500, 10, 1),
+            nn.BatchNorm2d(10),
             nn.LeakyReLU(),
         )
 
         self.classifierA1 = nn.Sequential(
-            nn.Linear(10_000, 8_192),
+            nn.Linear(1_000, 1_800),
             nn.LeakyReLU(),
         )
 
         self.classifierB1 = nn.Sequential(
-            nn.Linear(8_192, 8_192),
+            nn.Linear(1_800, 1_800),
             nn.LeakyReLU(),
         )
 
 
         self.classifierC1 = nn.Sequential(
-            nn.Linear(8_192, 2_048),
+            nn.Linear(1_800, 1_800),
             nn.Tanh(),
         )
 
         self.classifierC2 = nn.Sequential(
-            nn.Linear(2_048, 1_024),
+            nn.Linear(1_800, 1_800),
             nn.LeakyReLU(),
         )
 
         self.classifierD1 = nn.Sequential(
-            nn.Linear(1_024, class_count),
+            nn.Linear(1_800, class_count),
             nn.Softmax(dim=1)
         )
 
@@ -78,6 +88,7 @@ class CNN(nn.Module):
         x = self.FeatureA1(x)
         x = self.FeatureA2(x)
         x = self.FeatureA3(x)
+        x = self.FeatureA4(x)
 
         x = torch.flatten(x, 1)
 
