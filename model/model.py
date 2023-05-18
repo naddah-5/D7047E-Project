ec602ecbaeccddcb882217e8206d2ed8432b20c4
naddah-5 <andersoskardahl@hotmail.com>
2023-05-18 12:09:53 +0200

    dropout might have been applied incorrectly

--- model/model.py
+++ model/model.py
@@ -6,7 +6,8 @@ class CNN(nn.Module):
     def __init__(self, class_count: int = 2, dropout: float = 0.6, drop_in: float = 0.8, device: str = 'cpu'):
         super().__init__()
 
-
+        self.dropin = drop_in
+        self.dropout = dropout
         
         self.A1 = nn.Sequential(
             nn.Conv2d(1, 1, 1),
@@ -22,51 +23,46 @@ class CNN(nn.Module):
             nn.BatchNorm2d(1),
             nn.LeakyReLU(),
 
-            nn.Conv2d(1, 300, 5),
-            nn.BatchNorm2d(300),
+            nn.Conv2d(1, 500, 5),
+            nn.BatchNorm2d(500),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
 
-            nn.Conv2d(300, 1, 1),
+            nn.Conv2d(500, 1, 1),
             nn.BatchNorm2d(1),
             nn.LeakyReLU(),
 
-            nn.Conv2d(1, 300, 3),
-            nn.BatchNorm2d(300),
+            nn.Conv2d(1, 500, 3),
+            nn.BatchNorm2d(500),
             nn.LeakyReLU(),
 
-            nn.Conv2d(300, 100, 1),
+            nn.Conv2d(500, 100, 1),
             nn.BatchNorm2d(100),
             nn.LeakyReLU(),
         )
 
-        self.classifier = nn.Sequential(
-            
+        self.classifierA = nn.Sequential(
             nn.Linear(10_000, 8_192),
             nn.LeakyReLU(),
-            nn.Dropout(p=drop_in),
+        )
 
+        self.classifierB = nn.Sequential(
             nn.Linear(8_192, 8_192),
             nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
+        )
 
-            nn.Linear(8_192, 4_096),
+        self.classifierC = nn.Sequential(
+            nn.Linear(8_192, 2_048),
+            nn.Tanh(),
+        )
+
+        self.classifierD = nn.Sequential(
+            nn.Linear(2_048, 1_024),
             nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
+        )
 
-            nn.Linear(4_096, 2_048),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(2_048, 2_048),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(2_048, 512),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(512, class_count),
+        self.classifierD = nn.Sequential(
+            nn.Linear(1_024, class_count),
             nn.Softmax(dim=1)
         )
 
@@ -76,6 +72,16 @@ class CNN(nn.Module):
         x = self.A1(x)
 
         x = torch.flatten(x, 1)
-        x = self.classifier(x)
+
+        x = self.classifierA(x)
+        nn.Dropout(p=self.dropin),
+
+        x = self.classifierB(x)
+        nn.Dropout(p=self.dropout),
+
+        x = self.classifierC(x)
+        nn.Dropout(p=self.dropout),
+
+        x = self.classifierD(x)
         return x
     
