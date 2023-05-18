ad8a8ed149e75619f414a9161d1ca8e9da1051b4
naddah-5 <andersoskardahl@hotmail.com>
2023-05-18 21:15:39 +0200

    wip

--- model/model.py
+++ model/model.py
@@ -3,42 +3,45 @@ import torch.nn as nn
 import copy
 
 class CNN(nn.Module):
-    def __init__(self, class_count: int = 2, dropout: float = 0.8, drop_in: float = 0.95, device: str = 'cpu'):
+    def __init__(self, class_count: int = 2, dropout: float = 0.6, drop_in: float = 0.8, drop_tail = 0.4, device: str = 'cpu'):
         super().__init__()
 
         self.dropin = nn.Dropout(p=drop_in)
         self.dropout = nn.Dropout(p=dropout)
+        self.droptail = nn.Dropout(p=drop_tail)
         
         self.FeatureA1 = nn.Sequential(
-            nn.Conv2d(1, 300, 7),
-            nn.BatchNorm2d(300),
+            nn.Conv2d(1, 1, 1),
+            nn.BatchNorm2d(1),
+            nn.Conv2d(1, 200, 7),
+            nn.BatchNorm2d(200),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
             
-            nn.Conv2d(300, 1, 1),
+            nn.Conv2d(200, 1, 1),
             nn.BatchNorm2d(1),
             nn.LeakyReLU(),
         )
 
 
         self.FeatureA2 = nn.Sequential(
-            nn.Conv2d(1, 500, 5),
-            nn.BatchNorm2d(500),
+            nn.Conv2d(1, 400, 5),
+            nn.BatchNorm2d(400),
             nn.LeakyReLU(),
             nn.MaxPool2d(4, 4),
 
-            nn.Conv2d(500, 1, 1),
+            nn.Conv2d(400, 1, 1),
             nn.BatchNorm2d(1),
             nn.LeakyReLU(),
             )
 
 
         self.FeatureA3 = nn.Sequential(
-            nn.Conv2d(1, 500, 3),
-            nn.BatchNorm2d(500),
+            nn.Conv2d(1, 400, 3),
+            nn.BatchNorm2d(400),
             nn.LeakyReLU(),
 
-            nn.Conv2d(500, 100, 1),
+            nn.Conv2d(400, 100, 1),
             nn.BatchNorm2d(100),
             nn.LeakyReLU(),
         )
@@ -48,22 +51,23 @@ class CNN(nn.Module):
             nn.LeakyReLU(),
         )
 
-        self.classifierA2 = nn.Sequential(
+        self.classifierB1 = nn.Sequential(
             nn.Linear(8_192, 8_192),
             nn.LeakyReLU(),
         )
 
-        self.classifierA3 = nn.Sequential(
+
+        self.classifierC1 = nn.Sequential(
             nn.Linear(8_192, 2_048),
             nn.Tanh(),
         )
 
-        self.classifierA4 = nn.Sequential(
+        self.classifierC2 = nn.Sequential(
             nn.Linear(2_048, 1_024),
             nn.LeakyReLU(),
         )
 
-        self.classifierA5 = nn.Sequential(
+        self.classifierD1 = nn.Sequential(
             nn.Linear(1_024, class_count),
             nn.Softmax(dim=1)
         )
@@ -79,13 +83,13 @@ class CNN(nn.Module):
 
         x = self.dropin(self.classifierA1(x))
         
-        x = self.dropout(self.classifierA2(x))
+        x = self.dropout(self.classifierB1(x))
 
-        x = self.dropout(self.classifierA3(x))
+        x = self.droptail(self.classifierC1(x))
 
-        x = self.dropout(self.classifierA4(x))
+        x = self.droptail(self.classifierC2(x))
 
-        x = self.classifierA5(x)
+        x = self.classifierD1(x)
         
         return x
     
