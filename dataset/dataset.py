805e09a850276d4886fdcdf5feaa8ed75245539f
Lucke0011 <lukas.ledin@gmail.com>
2023-05-19 13:21:21 +0200

    latest dataset.py update

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,8 +2,8 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+import ssl
 from torch.utils.data import WeightedRandomSampler
-
 
 def load_dataset(scale: list = [224, 224], batch_size: int = 10):
     transform = transforms.Compose([
@@ -17,26 +17,36 @@ def load_dataset(scale: list = [224, 224], batch_size: int = 10):
     valset = datasets.ImageFolder('dataset/chest_xray/val', transform)
     testset = datasets.ImageFolder('dataset/chest_xray/test', transform)
 
-    class_labels = trainset.targets
-    num_normal = class_labels.count(0)
-    num_pneumonia = class_labels.count(1)
-    min_len = min(num_normal, num_pneumonia)
-    labels = [0] * min_len + [1] * min_len
-    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
-
+    weights, labels = sample2(trainset, 1)
     train_sampler = WeightedRandomSampler(weights, len(labels), replacement=False)
 
-    class_labels = testset.targets
-    num_normal = class_labels.count(0)
-    num_pneumonia = class_labels.count(1)
-    min_len = min(num_normal, num_pneumonia)
-    labels = [0] * min_len + [1] * min_len
-    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
-
+    weights, labels = sample2(testset, 1)
     test_sampler = WeightedRandomSampler(weights, len(labels), replacement=False)
 
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, sampler=train_sampler, num_workers=2)
     validation_loader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=2)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, sampler=test_sampler, num_workers=2)
 
-    return train_loader, validation_loader, test_loader
+    return train_loader, validation_loader, test_loader, labels
+
+
+
+# size is the % of total dataset. Ex. 1 = 100% and 0.5 = 50%
+def sample2(dataset, size):
+    class_labels = dataset.targets
+    num_class0 = class_labels.count(0)
+    num_class1 = class_labels.count(1)
+    min_len = min(num_class0, num_class1) * size
+    labels = [0] * min_len + [1] * min_len
+    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 2)
+
+    return weights, labels
+
+def sample3(dataset, size):
+    class_labels = dataset.targets
+    num_class0 = class_labels.count(0)
+    num_class1 = class_labels.count(1)
+    num_class2 = class_labels.count(2)
+    min_len = min(num_class0, num_class1, num_class2) * size
+    labels = [0] * min_len + [1] * min_len + [2] * min_len
+    weights = torch.DoubleTensor([1.0 / min_len] * min_len * 3)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from dataset.dataset import Dataset
+# from dataset.dataset import Dataset
 from model.model import CNN
 from application.train import Training
 from application.test import test_model
@@ -7,25 +7,23 @@ import torch
 from dataset.dataset import load_dataset
 
 
-def main(epochs: int = 100, batch_size: int = 16, learning_rate: float = 1e-4):
+def main(epochs: int = 1, batch_size: int = 10, learning_rate: float = 0.0001):
     best_net: str = ''
 
-    my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # set device to gpu if available
+    my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # set device to gpu if available
     print("using device ", my_device)
-    
-    train_loader, validation_loader, test_loader= load_dataset(batch_size=batch_size)
-    
+
+    train_loader, validation_loader, test_loader, _ = load_dataset(batch_size=batch_size)
 
     model = CNN(class_count=2, device=my_device)
 
-
     training = Training(model, train_loader, validation_loader, test_loader, epochs, learning_rate, device=my_device)
     training.train_model()
-    
 
     test_accuracy, f1 = test_model(test_loader=test_loader, network=model, device=my_device)
 
     print("\nTest accuracy: %f" % test_accuracy, "\nTest f1: %f" % f1)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
