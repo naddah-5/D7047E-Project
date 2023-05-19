79cc74a0309d8338db73e1a2d4ef5f48dcddf8b8
daavsan <david.kwiatkowski00@gmail.com>
2023-05-19 20:03:41 +0200

    standard deconv working

--- model/deconv_model.py
+++ model/deconv_model.py
@@ -9,61 +9,51 @@ class DeconvCNN(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         #self.droptail = nn.Dropout(p=drop_tail)
         
+        
         self.deFeatureA1 = nn.Sequential(
-            nn.BatchNorm2d(200),
-            nn.ConvTranspose2d(200, 1, 7),
-            nn.BatchNorm2d(1),
+            nn.ConvTranspose2d(100, 1, 7),
             nn.ConvTranspose2d(1, 1, 1),
-            nn.LeakyReLU(),
-
         )
-        self.unpool1_5 = nn.MaxUnpool2d(4, 4)
+        self.unpoolA1 = nn.MaxUnpool2d(2, 2)
 
         self.deFeatureA1_5 = nn.Sequential(
-            nn.BatchNorm2d(1),
-            nn.ConvTranspose2d(1, 200, 1),
-            nn.LeakyReLU(),
+            nn.ConvTranspose2d(100, 100, 5),
         )
+        self.unpoolA1_5 = nn.MaxUnpool2d(2, 2)
 
-        self.deFeatureA2 = nn.Sequential(
-            nn.BatchNorm2d(400),
-            nn.ConvTranspose2d(400, 1, 5),
-            nn.LeakyReLU(),
-            )
+        self.deFeatureA1_5_5 = nn.Sequential(
+            nn.ConvTranspose2d(100, 100, 3),
+        )
+        self.unpoolA1_5_5 = nn.MaxUnpool2d(2, 2)
+
+        self.deFeatureB1 = nn.Sequential(
+            nn.ConvTranspose2d(500, 100, 3),
+        )
+        self.unpoolB1 = nn.MaxUnpool2d(2, 2)
+
+        self.deFeatureB1_5 = nn.Sequential(
+            nn.ConvTranspose2d(500, 500, 3),
+        )
+        self.unpoolB1_5 = nn.MaxUnpool2d(2, 2)
         
-        self.unpool2_5 = nn.MaxUnpool2d(4, 4)
-
-        self.deFeatureA2_5 = nn.Sequential(
-            nn.BatchNorm2d(1),
-            nn.ConvTranspose2d(1, 400, 1),
-            nn.LeakyReLU(),
-            )
-
-
-        self.deFeatureA3 = nn.Sequential(
-            
-            nn.BatchNorm2d(100),
-            nn.ConvTranspose2d(100, 400, 1),
-            nn.LeakyReLU(),
-            nn.BatchNorm2d(400),
-            nn.ConvTranspose2d(400, 1, 3),
-            nn.LeakyReLU(),
-           
-        )
-
         self.to(device)
         
 
     def forward(self, x: torch.Tensor, pool_indices, sizes) -> torch.Tensor:
         
-        x = self.deFeatureA3(x)
+        x = self.unpoolB1_5(x, pool_indices[4], sizes[4])
+        x = self.deFeatureB1_5(x)
+        
+        x = self.unpoolB1(x, pool_indices[3], sizes[3])
+        x = self.deFeatureB1(x)
 
-        x = self.deFeatureA2_5(x)
-        x = self.unpool2_5(x, pool_indices[1], sizes[1])
-        x = self.deFeatureA2(x)
+        x = self.unpoolA1_5_5(x, pool_indices[2], sizes[2])
+        x = self.deFeatureA1_5_5(x)
 
+        x = self.unpoolA1_5(x, pool_indices[1], sizes[1])
         x = self.deFeatureA1_5(x)
-        x = self.unpool1_5(x, pool_indices[0], sizes[0])
+
+        x = self.unpoolA1(x, pool_indices[0], sizes[0])
         x = self.deFeatureA1(x)
         
         return x
--- model/model.py
+++ model/model.py
@@ -3,111 +3,94 @@ import torch.nn as nn
 import copy
 
 class CNN(nn.Module):
-    def __init__(self, class_count: int = 2, dropout: float = 0.6, drop_in: float = 0.8, drop_tail = 0.4, device: str = 'cpu'):
+    def __init__(self, class_count: int = 2, dropout: float = 0.8, drop_in: float = 0.9, device: str = 'cpu'):
         super().__init__()
 
-        self.dropin = nn.Dropout(p=drop_in)
-        self.dropout = nn.Dropout(p=dropout)
-        self.droptail = nn.Dropout(p=drop_tail)
+
         
-        self.FeatureA1 = nn.Sequential(
+        self.A1 = nn.Sequential(
             nn.Conv2d(1, 1, 1),
-            nn.BatchNorm2d(1),
-            nn.Conv2d(1, 200, 7),
-            nn.BatchNorm2d(200),
-            nn.LeakyReLU(),
+            nn.Conv2d(1, 100, 7),
         )
-        self.pool1 = nn.MaxPool2d(4, 4, return_indices=True)
+        self.poolA1 = nn.MaxPool2d(2, 2, return_indices=True)
+
+        self.A1_5 = nn.Sequential(
+            nn.Conv2d(100, 100, 5),
+        )
+        self.poolA1_5 = nn.MaxPool2d(2, 2, return_indices=True)
+
+        self.A1_5_5 = nn.Sequential(
+            nn.Conv2d(100, 100, 3),
+        )
+        self.poolA1_5_5 = nn.MaxPool2d(2, 2, return_indices=True)
+
+        self.B1 = nn.Sequential(
+            nn.Conv2d(100, 500, 3),                 # B1
+        )
+        self.poolB1 = nn.MaxPool2d(2, 2, return_indices=True)
         
-        self.FeatureA1_5 = nn.Sequential(
-            nn.Conv2d(200, 1, 1),
-            nn.BatchNorm2d(1),
-            nn.LeakyReLU(),
+        self.B1_5 = nn.Sequential(
+            nn.Conv2d(500, 500, 3),
         )
+        self.poolB1_5 = nn.MaxPool2d(2, 2, return_indices=True)
 
 
-        self.FeatureA2 = nn.Sequential(
-            nn.Conv2d(1, 400, 5),
-            nn.BatchNorm2d(400),
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=drop_in),
+            
+            nn.Linear(8_000, 8_000),                 # fc1
             nn.LeakyReLU(),
-            )
-        
-        self.pool2 = nn.MaxPool2d(4, 4, return_indices=True)
+            nn.Dropout(p=dropout),
 
-        self.FeatureA2_5 = nn.Sequential(
-            nn.Conv2d(400, 1, 1),
-            nn.BatchNorm2d(1),
+            nn.Linear(8_000, 8_000),
             nn.LeakyReLU(),
-            )
+            nn.Dropout(p=dropout),
 
-
-        self.FeatureA3 = nn.Sequential(
-            nn.Conv2d(1, 400, 3),
-            nn.BatchNorm2d(400),
+            nn.Linear(8_000, 5_000),
             nn.LeakyReLU(),
+            nn.Dropout(p=dropout),
 
-            nn.Conv2d(400, 100, 1),
-            nn.BatchNorm2d(100),
+            nn.Linear(5_000, 5_000),
             nn.LeakyReLU(),
-        )
+            nn.Dropout(p=dropout),
 
-
-
-        self.classifierA1 = nn.Sequential(
-            nn.Linear(10_000, 8_192),
+            nn.Linear(5_000, 1_000),
             nn.LeakyReLU(),
-        )
+            nn.Dropout(p=dropout),
 
-        self.classifierB1 = nn.Sequential(
-            nn.Linear(8_192, 8_192),
-            nn.LeakyReLU(),
-        )
-
-
-        self.classifierC1 = nn.Sequential(
-            nn.Linear(8_192, 2_048),
-            nn.Tanh(),
-        )
-
-        self.classifierC2 = nn.Sequential(
-            nn.Linear(2_048, 1_024),
-            nn.LeakyReLU(),
-        )
-
-        self.classifierD1 = nn.Sequential(
-            nn.Linear(1_024, class_count),
-            nn.Softmax(dim=1)
+            nn.Linear(1_000, class_count),             # fc5
         )
 
         self.to(device)
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         sizes = []
-        x = self.FeatureA1(x)
-        sizes.append(x.shape)
-        x , pool_indices1 = self.pool1(x)
-        x = self.FeatureA1_5(x)
 
-        x = self.FeatureA2(x)
+        x = self.A1(x)
         sizes.append(x.shape)
-        x , pool_indices2 = self.pool2(x)
-        x = self.FeatureA2_5(x)
-        
-        x = self.FeatureA3(x)
-        
+        x , pool_indices1  = self.poolA1(x)
+
+        x = self.A1_5(x)
+        sizes.append(x.shape)
+        x , pool_indices2  = self.poolA1_5(x)
+
+        x = self.A1_5_5(x)
+        sizes.append(x.shape)
+        x , pool_indices3  = self.poolA1_5_5(x)
+
+        x = self.B1(x)
+        sizes.append(x.shape)
+        x , pool_indices4  = self.poolB1(x)
+
+        x = self.B1_5(x)
+        sizes.append(x.shape)
+        x , pool_indices5  = self.poolB1_5(x)
+
         our_features = x
         x = torch.flatten(x, 1)
-
-        x = self.dropin(self.classifierA1(x))
+        x = self.classifier(x)
         
-        x = self.dropout(self.classifierB1(x))
-
-        x = self.droptail(self.classifierC1(x))
-
-        x = self.droptail(self.classifierC2(x))
-
-        x = self.classifierD1(x)
-
-        pool_indices = (pool_indices1, pool_indices2)
+        
+        pool_indices = (pool_indices1, pool_indices2, pool_indices3, pool_indices4, pool_indices5)
         return x, our_features, pool_indices, sizes
-    
